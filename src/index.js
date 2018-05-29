5b3e3b610b245248aef3013daac7f63a890ce7b0
kukuen <mister.qqn@gmail.com>
2018-05-29 19:27:10 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -1,34 +1,82 @@
-import ffmpeg  from 'fluent-ffmpeg'
-import path from 'path'
-import child_process from 'child_process'
-import _ from 'lodash'
-import fs from 'fs-extra'
+import path from 'path';
+import child_process from 'child_process';
+import _ from 'lodash';
+import fs from 'fs-extra';
+//import yargs from 'yargs';
 
-const ROOT = 'E:\\SteamLibrary\\steamapps\\common\\Beat Saber\\CustomSongs'
+const FFMPEG_PATH = path.join(process.cwd(), 'ffmpeg/ffmpeg.exe');
+//
+//let argv = yargs
+//    .option('i', {
+//        alias: 'input',
+//        demandOption: true,
+//        describe: 'path to the difficulty .json file',
+//        type: 'string'
+//    })
+//    .option('s', {
+//        alias: 'start',
+//        demandOption: true,
+//        describe: 'beat to start the slice at',
+//        type: 'number'
+//    })
+//    .option('e', {
+//        alias: 'end',
+//        describe: 'beat to end the slice at',
+//        default: null,
+//        type: 'number'
+//    })
+//    .option('r', {
+//        alias: 'repeat',
+//        describe: 'repeat the slice [n] times',
+//        default: 1,
+//        type: 'number'
+//    })
+//    .option('silence', {
+//        describe: 'add [n] empty beats before each slice',
+//        default: 4,
+//        type: 'number'
+//    })
+//    .argv
 
+console.log(argv)
 
-async function slice({songName, difficulty, startBeat, endBeat = null, repeatCount = 1, silentBeats = 2}) {
-    let srcDir = path.join(ROOT, songName);
-    let destDir = path.join(ROOT, '__' + songName + ' (sliced)');
-    let info = JSON.parse(await fs.readFile(path.join(srcDir, 'info.json'), 'utf8'))
+console.log(process.cwd());
+
+slice({
+    songFile: argv.input,
+    startBeat: argv.start,
+    endBeat: argv.end,
+    repeatCount: argv.repeat,
+    silentBeats: argv.silence
+})
+    .then(() => console.log('complete'))
+    .catch(err => console.error(err));
+
+async function slice({songFile, startBeat, endBeat = null, repeatCount = 1, silentBeats = 4}) {
+    let difficulty = path.basename(songFile, '.json');
+    let srcDir = path.dirname(songFile);
+    let songName = path.basename(srcDir)
+    let destDir = path.join(path.dirname(srcDir), '(sliced) ' + songName);
+
+    let info = JSON.parse(await fs.readFile(path.join(srcDir, 'info.json'), 'utf8'));
 
     let newInfo = {
         ...info,
         songName: '(sliced) ' + info.songName,
         difficultyLevels: info.difficultyLevels.filter(d => d.difficulty === difficulty)
-    }
+    };
 
-    if (newInfo.difficultyLevels.length === 0) throw new Error('No difficulty ' + difficulty + ' found')
+    if (newInfo.difficultyLevels.length === 0) throw new Error('No difficulty ' + difficulty + ' found');
 
-    let difficultyLevel = newInfo.difficultyLevels[0]
+    let difficultyLevel = newInfo.difficultyLevels[0];
 
-    let beatmap = JSON.parse(await fs.readFile(path.join(srcDir, difficultyLevel.jsonPath), 'utf8'))
+    let beatmap = JSON.parse(await fs.readFile(songFile, 'utf8'));
 
     let bpm = beatmap._beatsPerMinute;
 
-    let silenceDuration = silentBeats * 60 / bpm
-    let startTime = startBeat * 60 / bpm
-    let endTime = endBeat != null ? endBeat * 60 / bpm : null
+    let silenceDuration = silentBeats * 60 / bpm;
+    let startTime = startBeat * 60 / bpm;
+    let endTime = endBeat != null ? endBeat * 60 / bpm : null;
 
     let baseNewBeatmap = {
         ...beatmap,
@@ -41,130 +89,148 @@ async function slice({songName, difficulty, startBeat, endBeat = null, repeatCou
         _notes: beatmap._notes
             .filter(filterByTime(startBeat, endBeat))
             .map(addTime(-(startBeat - silentBeats)))
-        ,
-    }
+
+    };
 
     let parts = [
         {
-            duration: silenceDuration
+            duration: silenceDuration / 2
         },
         {
             source: path.join(srcDir, difficultyLevel.audioPath),
-            startTime: startTime,
-            duration: endTime ? endTime - startTime : null
+            startTime: startTime - silenceDuration / 2,
+            fadeIn: silenceDuration / 2,
+            fadeOut: silenceDuration / 2,
+            duration: endTime ? endTime - startTime + silenceDuration : null
         }];
-
 
     let newBeatmap = baseNewBeatmap;
 
     if (repeatCount > 1) {
         let loopDuration = silentBeats + endBeat - startBeat;
 
-        for (var i = 1; i < repeatCount; i++) {
+        for (let i = 1; i < repeatCount; i++) {
             newBeatmap._obstacles = [
                 ...newBeatmap._obstacles,
                 ...baseNewBeatmap._obstacles.map(addTime(loopDuration * i))
-            ]
+            ];
             newBeatmap._events = [
                 ...newBeatmap._events,
                 ...baseNewBeatmap._events.map(addTime(loopDuration * i))
-            ]
+            ];
             newBeatmap._notes = [
                 ...newBeatmap._notes,
                 ...baseNewBeatmap._notes.map(addTime(loopDuration * i))
-            ]
+            ];
             parts = [
                 ...parts,
                 {
-                    duration: silenceDuration
-                },
-                {
                     source: path.join(srcDir, difficultyLevel.audioPath),
-                    startTime: startTime,
-                    duration: endTime ? endTime - startTime : null
+                    startTime: startTime - silenceDuration / 2,
+                    fadeIn: silenceDuration / 2,
+                    fadeOut: silenceDuration / 2,
+                    duration: endTime ? endTime - startTime + silenceDuration : null
                 }
-            ]
+            ];
         }
     }
 
-    await fs.ensureDir(destDir)
-    await fs.writeFile(path.join(destDir, difficultyLevel.jsonPath), JSON.stringify(newBeatmap))
-    await fs.writeFile(path.join(destDir, 'info.json'), JSON.stringify(newInfo))
+    await fs.ensureDir(destDir);
+    await fs.writeFile(path.join(destDir, difficultyLevel.jsonPath), JSON.stringify(newBeatmap));
+    await fs.writeFile(path.join(destDir, 'info.json'), JSON.stringify(newInfo));
     await concat({
         parts: parts,
         output: path.join(destDir, difficultyLevel.audioPath)
-    })
+    });
 }
 
 function addTime(delta) {
-    return e => ({...e, _time: e._time + delta})
+    return e => ({...e, _time: e._time + delta});
 }
 
 function filterByTime(startTime, endTime) {
-    return e => e._time >= startTime && (endTime == null || e._time < endTime)
+    return e => e._time >= startTime && (endTime == null || e._time < endTime);
 }
-
-slice({songName: 'Invader Invader', difficulty: 'Expert', startBeat: 197, endBeat: 229, repeatCount: 1})
-
 
 async function concat({parts, output}) {
     return new Promise((resolve, reject) => {
-        let filters = []
-        let filtersParts = []
         let sources = _.chain(parts)
-            .flatMap((p, i) => {
-                if (p.source == null) {
-                    filtersParts.push('[' + i + ']')
-                    return [
-                        '-f', 'lavfi',
-                        '-t', toFfmpegTime(p.duration),
-                        '-i', 'anullsrc=channel_layout=stereo:sample_rate=44000'
-                    ]
-                } else {
-                    filters.push('[' + i + ']afade=t=in:st=0:d=1[a' + i + ']')
-                    filtersParts.push('[a' + i + ']')
-                    let res = [];
+            .map(part => {
+                if (part.source == null) {
+                    if (part.duration > 0) {
+                        return {
+                            input: [
+                                '-f', 'lavfi',
+                                '-t', toFfmpegTime(part.duration),
+                                '-i', 'anullsrc=channel_layout=stereo:sample_rate=44000'
+                            ],
+                            filters: []
+                        };
+                    }
+                    else {
+                        return null;
+                    }
+                }
+                else {
+                    let input = [];
+                    let filters = [];
 
-                    if (p.startTime) res = [...res, '-ss', toFfmpegTime(p.startTime)]
-                    if (p.duration) res = [...res, '-t', toFfmpegTime(p.duration)]
+                    if (part.fadeIn) filters.push('afade=t=in:st=0:d=' + toFfmpegTime(part.fadeIn));
+                    if (part.fadeOut && part.duration) filters.push('afade=t=out:st=' + toFfmpegTime(part.duration - part.fadeOut) + ':d=' + toFfmpegTime(part.fadeOut));
 
-                    res = [...res, '-i', p.source]
+                    if (part.startTime) input.push('-ss', toFfmpegTime(part.startTime));
+                    if (part.duration) input.push('-t', toFfmpegTime(part.duration));
 
-                    return res
+                    input.push('-i', part.source);
+
+                    return {
+                        input: input,
+                        filters: filters
+                    };
                 }
             })
+            .filter(e => e != null)
             .value();
 
-        console.log(sources)
-        console.log(filters)
-        console.log(filtersParts)
+        let filters = sources
+            .map((s, i) => {
+                if (s.filters.length === 0) return null;
+                return '[' + i + ']' + s.filters.join(',') + '[a' + i + ']';
+            })
+            .filter(s => s != null);
 
-        let filtersStr = filters.join('; ')+';'
-        let filtersPartsStr = filtersParts.join(' ')
+        let concatSources = sources.map((s, i) => s.filters.length === 0 ? '[' + i + ']' : '[a' + i + ']').join('');
+        let concatFilter = concatSources + 'concat=n=' + sources.length + ':v=0:a=1';
+
+        let inputs = _.flatMap(sources, s => s.input);
+
+        filters.push(concatFilter);
 
         let args = [
-            ...sources,
-            '-filter_complex', filtersStr + ' ' + filtersPartsStr + ' concat=n=' + parts.length + ':v=0:a=1 ',
+            ...inputs,
+            '-filter_complex', filters.join(';'),
             '-y',
             output];
 
-        let concat = child_process.spawn(path.join(__dirname, '../ffmpeg/ffmpeg.exe'), args, {stdio: 'inherit'});
+        let concat = child_process.spawn(FFMPEG_PATH, args);
 
         concat.on('close', code => {
-            if (code === 0) resolve()
-            else reject(new Error('ffmpeg error code ' + code));
-        })
-    })
+            if (code === 0) {
+                resolve();
+            }
+            else {
+                reject(new Error('ffmpeg error code ' + code));
+            }
+        });
+    });
 }
 
 function toFfmpegTime(secs) {
-    let ms = Math.floor((secs * 1000) % 1000)
-    let s = Math.floor(secs % 60)
-    let m = Math.floor((secs / 60) % 60)
+    let ms = Math.floor((secs * 1000) % 1000);
+    let s = Math.floor(secs);
 
-    return m.toString().padStart(2, '0')
-        + ':' + s.toString().padStart(2, '0')
-        + '.' + ms.toString().padStart(3, '0')
+    return s.toString().padStart(2, '0')
+        + '.' + ms.toString().padStart(3, '0');
 }
 
 function getFormat(file) {
@@ -181,9 +247,13 @@ function getFormat(file) {
 
         decoder.stdout.on('data', d => str += d);
         decoder.on('close', code => {
-            if (code === 0)resolve(JSON.parse(str))
-            else reject(new Error('ffprobe error code ' + code));
-        })
-    })
+            if (code === 0) {
+                resolve(JSON.parse(str));
+            }
+            else {
+                reject(new Error('ffprobe error code ' + code));
+            }
+        });
+    });
 
 }
